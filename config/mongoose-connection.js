1020698999a9e2c05fc3451771b8c4357dcf6845
unknown <mr.singla2005@gmail.com>
2025-06-14 23:57:41 +0530

    add mongoose connection

---
+++ config/mongoose-connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
+const config = require("config");
+
+mongoose
+  .connect(`${config.get("MONGODB_URI")}`)
+  .then(function () {
+    dbgr("connected to mongoDB");
+  })
+  .catch(function (err) {
+    dbgr(err);
+  });
+
+module.exports = mongoose.connection;
