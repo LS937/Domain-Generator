be85ee3c5820b057f90ebccfbd3dfc7db78ab468
Lavish Singla <86468318+LS937@users.noreply.github.com>
2025-07-01 10:41:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -231,6 +231,6 @@ app.post("/delete-domain", isLoggedIn, async (req, res) => {
 
 let port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
